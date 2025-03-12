d29b01c7e8b77f10fc4acf6a42a2e70c16b08f34
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2025-03-13 00:22:24 +0700

    chore

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -1,12 +1,32 @@
-export const Homepage = ({error}: {error: any}) => {
+export const Homepage = ({ error }: { error: any }) => {
     return (
-        <div>
-            <form action="/diem" method="post">
-                <input type="text" name="username" placeholder="Username" />
-                <input type="password" name="password" placeholder="Password" />
-                <button type="submit">Submit</button>
-            </form>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-        </div>
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+                <title>Đăng nhập</title>
+            </head>
+            <body>
+                <div>
+                    <form action="/diem" method="post">
+                        <input
+                            type="text"
+                            name="username"
+                            placeholder="Username"
+                        />
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                        />
+                        <button type="submit">Submit</button>
+                    </form>
+                    {error && <p style={{ color: 'red' }}>{error}</p>}
+                </div>
+            </body>
+        </html>
     );
 };
--- src/Result.tsx
+++ src/Result.tsx
@@ -2,6 +2,11 @@ export const Result = ({ dsDiemHocKy }: { dsDiemHocKy: any }) => {
     return (
         <html>
             <head>
+                <meta charset="UTF-8" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
                 <title>Danh sách điểm</title>
                 <style>
                     {`
