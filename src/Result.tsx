6b009be993cbb0ec4e5cadcb9738e2628bf60fe5
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2025-03-13 00:43:33 +0700

    refactor: Result component

--- src/Result.tsx
+++ src/Result.tsx
@@ -36,41 +36,15 @@ export const Result = ({
                                 <th>Số tín chỉ</th>
                                 <th>Điểm thi</th>
                                 <th>Điểm giữa kỳ</th>
-                                <th>Điểm tk</th>
-                                <th>Điểm tk số</th>
-                                <th>Điểm tk chữ</th>
+                                <th>Điểm TK (10)</th>
+                                <th>Điểm TK (4)</th>
+                                <th>Điểm TK (chữ)</th>
                                 <th>Kết quả</th>
-                                <th>Trọng số, điểm</th>
+                                <th>Điểm thành phần</th>
                             </tr>
-                            {hocKy.ds_diem_mon_hoc.map((monHoc: any) => (
-                                <tr>
-                                    <td>{monHoc.ma_mon}</td>
-                                    <td>{monHoc.ten_mon}</td>
-                                    <td>{monHoc.so_tin_chi}</td>
-                                    <td>{monHoc.diem_thi ?? ''}</td>
-                                    <td>{monHoc.diem_giua_ky ?? ''}</td>
-                                    <td>{monHoc.diem_tk ?? ''}</td>
-                                    <td>{monHoc.diem_tk_so ?? ''}</td>
-                                    <td>{monHoc.diem_tk_chu ?? ''}</td>
-                                    <td>
-                                        {monHoc.ket_qua ? 'Đạt' : 'Không đạt'}
-                                    </td>
-                                    <td>
-                                        {monHoc.ds_diem_thanh_phan.map(
-                                            (tp: any) => (
-                                                <>
-                                                    {tp.ten_thanh_phan}
-                                                    {': '}
-                                                    {tp.trong_so}
-                                                    {', '}
-                                                    {tp.diem_thanh_phan}
-                                                    <br />
-                                                </>
-                                            )
-                                        )}
-                                    </td>
-                                </tr>
-                            ))}
+                            {hocKy.ds_diem_mon_hoc.map((monHoc: any) =>
+                                MonHocRow(monHoc)
+                            )}
                         </table>
                     </>
                 ))}
@@ -78,3 +52,47 @@ export const Result = ({
         </html>
     );
 };
+
+function MonHocRow(monHoc: any) {
+    return (
+        <tr>
+            <td>{monHoc.ma_mon}</td>
+            <td>{monHoc.ten_mon}</td>
+            <td>{monHoc.so_tin_chi}</td>
+            <td>{monHoc.diem_thi ?? ''}</td>
+            <td>{monHoc.diem_giua_ky ?? ''}</td>
+            <td>{monHoc.diem_tk ?? ''}</td>
+            <td>{monHoc.diem_tk_so ?? ''}</td>
+            <td>{monHoc.diem_tk_chu ?? ''}</td>
+            <td>{monHoc.ket_qua ? 'Đạt' : 'Không đạt'}</td>
+            <td>{DiemThanhPhanTable(monHoc)}</td>
+        </tr>
+    );
+}
+
+function DiemThanhPhanTable(monHoc: any) {
+    if (!monHoc.ds_diem_thanh_phan || monHoc.ds_diem_thanh_phan.length === 0) {
+        return null;
+    }
+    
+    return (
+        <table border={1}>
+            <thead>
+                <tr>
+                    <th>Thành phần</th>
+                    <th>Trọng số</th>
+                    <th>Điểm</th>
+                </tr>
+            </thead>
+            <tbody>
+                {monHoc.ds_diem_thanh_phan.map((tp: any) => (
+                    <tr>
+                        <td>{tp.ten_thanh_phan}</td>
+                        <td>{tp.trong_so}</td>
+                        <td>{tp.diem_thanh_phan}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+}
